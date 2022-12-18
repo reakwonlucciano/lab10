e84b8a5b79bb0b4a4b176a0b2c3fc65d3f1d1e6c
Reakwon Lucciano <62098320+reakwonlucciano@users.noreply.github.com>
2022-12-18 16:19:41 +0000

    Update edit.js

--- src/components/edit.js
+++ src/components/edit.js
@@ -1,67 +1,94 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React from 'react'
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-export function Edit(){
-    let {id} = useParams();
+export function Edit(props) {
+
+    /**The useParams hook returns an object of key/value pairs of
+     * the dynamic params from the current URL that were matched by
+     * the <Route path>
+     */
+    let { id } = useParams();
+
+    /**update arrays using the React useState()
+     * and without the Array object's push() method
+     */
     const [title, setTitle] = useState('');
     const [cover, setCover] = useState('');
     const [author, setAuthor] = useState('');
 
-    useEffect(()=>{
-        axios.get('http://localhost:4000/api/book/'+id)
-        .then((response)=>{
-            setTitle(response.data.title);
-            setCover(response.data.cover);
-            setAuthor(response.data.author);
-        })
-        .catch()
-    },[]);
+    //useNavigate return a function that we can use to navigate
+    const navigate = useNavigate();
 
-    const handleSubmit = (e)=>{
-        e.preventDefault();
+    //useEffect Hook which is similar to componentDidMount
+    useEffect(() => {
+        /**axios is a promised based web client
+         * make a  HTTP Request with GET method and pass as part of the 
+         * URL
+         */
+        axios.get('http://localhost:4000/api/book/' + id)
+            .then((response) => {
+                //Assign Response data to the arrays using useState
+                setTitle(response.data.title);
+                setCover(response.data.cover);
+                setAuthor(response.data.author);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }, [])
 
-        const editBook = {
-            title:title,
-            cover:cover,
-            author:author
-        }
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
-        axios.put('http://localhost:4000/api/book/'+id,editBook)
-        .then()
-        .catch();
+        const newBook = {
+            id: id,
+            title: title,
+            cover: cover,
+            author: author
+        };
+
+        axios.put('http://localhost:4000/api/book/' + id, newBook)
+            .then((res) => {
+                console.log(res.data);
+                navigate('/read');
+            });
     }
 
-    return(
+    return (
         <div>
-            <h3>Edit component</h3>
+            <h2>Edit Component</h2>
             <form onSubmit={handleSubmit}>
-            <div className="form-group">
-                        <label>Edit Book Title: </label>
-                        <input type="text"
-                            className="form-control"
-                            value={title}
-                            onChange={(e)=>{setTitle(e.target.value)}}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Edit Book Cover: </label>
-                        <input type="text"
-                            className="form-control"
-                            value={cover}
-                            onChange={(e)=>{setCover(e.target.value)}}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Edit Book Author: </label>
-                        <input type="text"
-                            className="form-control"
-                            value={author}
-                            onChange={(e)=>{setAuthor(e.target.value)}}
-                        />
-                    </div>
-                <input type="submit" value="Edit Book"></input>
+
+                <div className="form-group">
+                    <label>Add Book Title</label>
+                    <input type="text"
+                        className="form-control"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Add Release Year: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={cover}
+                        onChange={(e) => setCover(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Add Poster URL: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <input type="submit" value="Edit Book" className="btn btn-primary" />
+                </div>
             </form>
         </div>
-    );
+    )
+
 }
